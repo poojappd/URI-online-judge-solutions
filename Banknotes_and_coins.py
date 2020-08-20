2bdf3b97c414e178a38e7a5c8dc37a91bdc89e64
poojappd <48831727+poojappd@users.noreply.github.com>
2020-08-20 12:26:39 +0530

    Create Banknotes_and_coins.py

---
+++ Banknotes_and_coins.py
@@ -0,0 +1,22 @@
+notes=[100, 50, 20, 10, 5, 2]
+coins=[1, 0.50, 0.25, 0.10, 0.05,0.01]
+amount=float(input())
+print("NOTAS:")
+if amount>=0 and amount<= 1000000.00:
+    for i in notes:
+        if amount>=i:
+            x=int(amount//i)
+            amount=amount-(i*x)
+            print("{} nota(s) de R$ {:.2f}".format(int(x),i))
+        else:    
+            print("0 nota(s) de R$ {:.2f}".format(i))
+print("\nMOEDAS:")
+for j in coins:
+        if amount>=j:
+            y=int(amount//j)
+            amount=round(amount-(j*y),4)
+            
+            print("{} moeda(s) de R$ {:.2f}".format(int(y),j))
+        else:    
+            print("0 moeda(s) de R$ {:.2f}".format(j))
+        
